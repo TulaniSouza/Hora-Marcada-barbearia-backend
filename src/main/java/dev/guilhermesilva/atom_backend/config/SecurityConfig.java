8b6152706867be874b30b0709987f765d2940164
Tulani Souza <tulanidepaula@gmail.com>
2026-06-17 16:16:37 -0300

    fix: permit root URL access and configure Swagger redirect

--- src/main/java/dev/guilhermesilva/atom_backend/config/SecurityConfig.java
+++ src/main/java/dev/guilhermesilva/atom_backend/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     private String corsAllowedOrigins;
 
     private static final String[] SWAGGER_WHITELIST = {
-            "/"
+            "/",
             "/swagger-ui.html",
             "/swagger-ui/**",
             "/v3/api-docs/**",
